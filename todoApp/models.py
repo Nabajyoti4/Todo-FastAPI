d594f5de3133a7754c4c317c259726b400708978
Nabajyoti4 <sirnaba@gmail.com>
2022-01-27 09:16:26 +0530

    todo model added

---
+++ todoApp/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base
+
+class Todos(Base):
+    __tablename__ = 'todos'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
